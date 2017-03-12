934faebaf8499d131a7f222f1fb2239d871d215d
exnester <mtutuncumacias1@gmail.com>
2017-03-12 18:13:15 -0400

    @rickyc

--- Homework 1/app.rb
+++ Homework 1/app.rb
@@ -11,18 +11,14 @@ ary.each do |row|
     if character== (character.to_i).to_s
       justnumbers.push(character)
     end
-
   end
-  #p justnumbers
   goodnumbers.push(justnumbers)
 end
 CSV.open("formednumbers.csv", "wb") do |csv|
   goodnumbers.each do |telephonenumber|
-    if telephonenumber.length==11
-
+    if telephonenumber.length==1
       csv << ["1-"+telephonenumber[1]+telephonenumber[2]+telephonenumber[3]+"-"+telephonenumber[4]+telephonenumber[5]+telephonenumber[6]+"-"+telephonenumber[7]+telephonenumber[8]+telephonenumber[9]+telephonenumber[10]]
     else
-
       csv << ["1-"+telephonenumber[0]+telephonenumber[1]+telephonenumber[2]+"-"+telephonenumber[3]+telephonenumber[4]+telephonenumber[5]+"-"+telephonenumber[6]+telephonenumber[7]+telephonenumber[8]+telephonenumber[9]]
     end
   end
